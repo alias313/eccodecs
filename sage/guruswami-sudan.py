3f5f6e5254f957419856b778b828c26fc4065a48
Elies <eliesgarciaalvira@gmail.com>
2025-02-21 18:52:49 +0100

    sagemath is awesome, actually

--- sage/guruswami-sudan.py
+++
@@ -1,7 +0,0 @@
-from sage.all import *
-
-F = GF(Integer(59))
-n, k = Integer(40), Integer(12)
-C = codes.GeneralizedReedSolomonCode(F.list()[:n], k)
-D = codes.decoders.GRSBerlekampWelchDecoder(C)
-print(D)
